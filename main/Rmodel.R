bd7b1afa289f911d1ba39247177fcfba5145f717
PierreRlld <84622002+PierreRlld@users.noreply.github.com>
2023-11-28 11:48:33 +0100

    up

---
+++ main/Rmodel.R
@@ -0,0 +1,18 @@
+# >> Dependencies
+require(lmtest)
+require(dynlm)
+require(forecast)
+library(vars)
+library(tsm)
+library(urca)
+library(sandwich)
+library(openxlsx)
+library(aTSA)
+library(car)
+library(urca)
+library(tsDyn)
+library(BVAR)
+#https://stackoverflow.com/questions/69639782/installing-gfortran-on-macbook-with-apple-m1-chip-for-use-in-r
+
+# >> DATA
+#===============================================================================
