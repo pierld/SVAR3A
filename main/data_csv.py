6e640e0c720fba61b7682db4d6c986609899f561
PierreRlld <84622002+PierreRlld@users.noreply.github.com>
2023-12-23 22:40:12 +0100

    up

--- main/data_csv.py
+++ main/data_csv.py
@@ -7,7 +7,7 @@ df_p_index = pd.read_excel("data/data_flat.xlsx",sheet_name="P_index")
 df_overall = pd.read_excel("data/overall_hicp.xlsx",sheet_name="overall")
 
 #%%
-df_w.to_csv("./data/df_w.csv")
-df_q_index.to_csv("./data/df_q_index.csv")
-df_p_index.to_csv("./data/df_p_index.csv")
-df_overall.to_csv("./data/df_overall.csv")
+df_w.to_csv("df_w.csv")
+df_q_index.to_csv("df_q_index.csv")
+df_p_index.to_csv("df_p_index.csv")
+df_overall.to_csv("df_overall.csv")
