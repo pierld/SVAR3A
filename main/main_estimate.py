c89371f2d14dd0ac122a6bc768f18233acff95fb
PierreRlld <84622002+pierld@users.noreply.github.com>
2025-02-13 13:41:52 +0100

    no update

--- main/main_estimate.py
+++ main/main_estimate.py
@@ -902,8 +902,8 @@ class CPIlabel:
 
 #%%
 #? =====================================================================
-#meta = CPIframe(df_q_index=df_q_index, df_p_index=df_p_index, df_w=df_w, country="France")
-#cpi = CPIlabel(meta=meta)
+meta = CPIframe(df_q_index=df_q_index, df_p_index=df_p_index, df_w=df_w, country="France")
+cpi = CPIlabel(meta=meta)
 #t1 = sector_estimation(meta=meta,col=21,shapiro_robust=True)
 
 
